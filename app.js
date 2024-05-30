55f0e1ed260d22db9a9aa969c744a42a9958c9f6
RichardAdeniji <140505706+RichardAdeniji@users.noreply.github.com>
2024-05-30 19:54:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.
+ADENIJI RICHARD ADEREMI
+VUG/CS/21/5353
